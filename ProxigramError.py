632dfb6109726fd0f56f4175d9921c24e10b8772
sakanak <vkanigicherla@gmail.com>
2020-11-13 02:28:35 -0500

    User file input

--- ProxigramError.py
+++ ProxigramError.py
@@ -5,12 +5,13 @@ import numpy
 # Use intermediate CSV file generated in ProxigramPeakDecom.py
 
 # On VK's system
-modpath = "steel2mod.csv"
-newpath = "steel2graph.csv"
+#modpath = "steel2mod.csv"
+#newpath = "steel2graph.csv"
 # isotopes to delete: Cr %,C2 %,C3 %,NiH %,P %,Si %,MoC %,H %
+# 1.7, 2.3, -4.9, -2.1
 
-#modpath = input("What was the filename of your intermediate CSV? Include .csv extension: ")
-#newpath = input("What was the filename of your final graphing CSV? Include .csv extension: ")
+modpath = input("What was the filename of your intermediate CSV? Include .csv extension: ")
+newpath = input("What was the filename of your final graphing CSV? Include .csv extension: ")
 
 dfmid = pandas.read_csv(modpath)
 dffin = pandas.read_csv(newpath)
@@ -45,7 +46,7 @@ dffin.insert (len(dffin.columns), "Error Sum", sum)
 
 del(dffin["Unnamed: 0"])
 
-dffin.to_csv("steel2error.csv")#input("What file would you like your error data to be written to? Include .csv extension: ")) 
+dffin.to_csv(input("What file would you like your error data to be written to? Include .csv extension: ")) 
 
 # print(dfmid.columns.get_loc("Distance (nm)")) # ----> col index
 # print(dfmid.columns[1]) # index ----> col name
@@ -218,4 +219,3 @@ print("                                                                         
 
 print(data)
 print(dfmid["Sample Count"].sum())
-# 1.7, 2.3, -4.9, -2.1
