a504975ba23eb0ed494d8ba250e6a00fb8cd6551
sakanak <vkanigicherla@gmail.com>
2020-12-08 01:07:28 -0500

    Gibbsian excess w/o density factor

---
+++ GibbsInterfacialExcess.py
@@ -0,0 +1,94 @@
+import pandas
+import numpy
+
+
+# PLEASE NOTE: THIS GIBBSIAN EXCESS CALCULATION IS INCORRECT, BECAUSE IT IS MISSING
+# A TERM FOR IDEAL DENSITY. THAT WILL BE UPDATED AS SOON AS POSSIBLE.
+
+modpath = input("What was the filename of your intermediate CSV? Include .csv extension: ")
+
+dfmid = pandas.read_csv(modpath)
+
+def rounding(x): # significant figure rounding
+    if(x == 0):
+        return 0
+    else:
+        return round(x, -int(numpy.floor(numpy.log10(numpy.abs(x)))))
+
+#########
+# Make colarray
+#########
+
+colarray = []
+
+for i in dfmid.columns:
+    colarray.append(i)
+    
+colarray.remove("Sample Count")
+colarray.remove("Distance (nm)")
+
+colarray.remove("Unnamed: 0")
+
+##############
+
+print(colarray)
+
+##############
+
+sumarray = []
+proparray = []
+
+for i in colarray:
+    sumarray.append(dfmid[i].sum())
+
+sum = 0
+for i in sumarray:
+    sum = sum + i
+
+for i in sumarray:
+    proparray.append((i/sum))
+
+data = [colarray, sumarray, proparray]
+
+print(proparray)
+
+print("Total Sample Count: " + str(dfmid["Sample Count"].sum()))
+
+
+
+######################
+# Gibbsian interfacial excess of solute calculation
+######################
+
+idx = 0
+head = ""
+head = input("What header would you like a Co for?: ")
+for i in range(0, len(colarray)):
+    if head == colarray[i]:
+        idx = i
+
+c0 = proparray[idx] 
+cnarray = []
+gibbsfinal = 0
+cN = 0
+gibbsI = 0
+delL = float('%.3g' % (dfmid["Distance (nm)"][1] - dfmid["Distance (nm)"][0])) 
+area = 0
+N = 0
+
+for i in range(0, len(dfmid["Distance (nm)"])):
+
+    n = dfmid["Sample Count"][i]
+    area = n/delL
+
+    cN = dfmid[head][i]/n
+    gibbsI = (cN-c0)/(1-c0)/area 
+    cnarray.append(gibbsI)
+
+    gibbsfinal = gibbsfinal + gibbsI
+    
+print("Gibbsian interfacial excess of " + head + " solute = " + str(gibbsfinal*-1) + " molecules/nm^2") # formula gives negative of excess, because cN - c0
+
+
+# print(dfmid.columns.get_loc("Distance (nm)")) # ----> col index
+# print(dfmid.columns[1]) # index ----> col name
