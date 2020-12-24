e4cdc3551dca1569c0598ce660a5be565bba0a2f
Vishal A. Kanigicherla <emplvk@KPSOFT.LOCAL>
2020-12-24 16:49:51 -0500

    Applet transfer

---
+++ applet/MyApplication.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+import GibbsInterfacialExcess, PosToCSV, ProxigramError, ProxigramPeakDecomp, ProxigramProfilePlot, ColumnModCheck, graph3d
+
+window = tk.Tk()
+window.title("GUI")
+
+label = tk.Label(window, text = "Hello World!").pack()
+window.geometry('400x200')
+
+
+
+def gibbsclicked():
+    print("clicked")
+    GibbsInterfacialExcess.getExcess()
+
+def colmodcheckclicked():
+    print("clicked")
+    ColumnModCheck.colcheck()
+    
+def postocsvclicked():
+    print("clicked")
+    PosToCSV.csvbuild()
+    
+def graphclicked(): 
+    print("clicked")
+    graph3d.plotpoints()
+    
+def proxplotclicked():
+    print("clicked")
+    ProxigramProfilePlot.proxplot()
+    
+def proxerrorclicked():
+    print("clicked")
+    ProxigramError.proxerror()
+
+
+def proxpeakdecompclicked():
+    print("clicked")
+    ProxigramPeakDecomp.entiredecomp()
+
+btcolmod = tk.Button(window, text = "Column Mod Check", command = colmodcheckclicked).pack()
+btgibbs = tk.Button(window, text = "Gibbsian Interfacial Excess", command = gibbsclicked).pack()
+btgraph = tk.Button(window, text = "Graph POS CSV", command = graphclicked).pack()
+btposcsv = tk.Button(window, text = "Create CSV from POS", command = postocsvclicked).pack()
+btproxerror = tk.Button(window, text = "Generate Proxigram Error CSV", command = proxerrorclicked).pack()
+btproxpeakdecomp = tk.Button(window, text = "Manual Proxigram Peak Decomposition", command = proxpeakdecompclicked).pack()
+btproxprofplot = tk.Button(window, text = "Generate Proxigram Profile Plot", command = proxplotclicked).pack()
+
+window.mainloop()
---
+++ applet/graph3d.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
+
+def plotpoints():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    n = 100
+
+    df = pd.read_csv("data\steelnew\steelnewpos.csv")
+
+    # For each set of style and range settings, plot n random points in the box
+    # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+    for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
+        xs = df["1"]
+        ys = df["2"]
+        zs = df["3"]
+        ax.scatter(xs, ys, zs, marker=m)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
