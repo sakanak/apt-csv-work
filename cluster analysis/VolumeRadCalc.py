828c58af23cab120d9bebaa6f1a104dfe138e986
KPSOFT\emplvk <vkanigicherla@gmail.com>
2021-02-02 20:36:18 -0500

    Initial commit

--- cluster analysis/VolumeRadCalc.py
+++ cluster analysis/VolumeRadCalc.py
@@ -16,12 +16,12 @@ else:
 
 print(bounded)
 
-molOmega = float(input("In nm^3, what is your omega parameter for the isotope of interest?: "))
-dMax = float(input("What is your d-max (nm)?: "))
-order = float(input("What is your order (ions)?: "))
-nMin = float(input("What is your N-min (ions)?: "))
-length = float(input("What is your l (nm)?: "))
-dErosion = float(input("What is your d-erosion paramter (nm)?: "))
+molOmega = float(input("In nm^3, what is your Average Atomic Volume for the isotope of interest?: "))
+#dMax = float(input("What is your d-max (nm)?: "))
+#order = float(input("What is your order (ions)?: "))
+#nMin = float(input("What is your N-min (ions)?: "))
+#length = float(input("What is your l (nm)?: "))
+#dErosion = float(input("What is your d-erosion paramter (nm)?: "))
 clusterCount = int(input("How many clusters do you have?: "))
 if(bounded == True):
     multiplier = 2
@@ -29,13 +29,14 @@ else:
     multiplier = 4
 
 
-df = pandas.read_csv(input("What's good?: "))
+df = pandas.read_csv(input("Input CSV filepath: "))
 
 #print(df)
 
-dfvolume = df["Solute Ions"][1:clusterCount+1]*multiplier*molOmega
-dfrad = (dfvolume*3/(4*numpy.pi))**(1/3)
+dfvolume = df["Solute Ions"][1:clusterCount+1]*multiplier*molOmega # Sphere volumes
+dfrad = (dfvolume*3/(4*numpy.pi))**(1/3) # Equivalent Radii (nm)
 
+stats = [numpy.std(dfvolume), numpy.std(dfrad)]
 
 volTotal = 0
 radTotal = 0
@@ -50,5 +51,6 @@ volAvg = volTotal/clusterCount
 radAvg = radTotal/clusterCount
 
 print("Average Volume is: " + str(volAvg) + " nm^3 and Average Radius is " + str(radAvg) + " nm." )
-
-
+print("Number of Features: " + str(clusterCount))
+print("Standard deviation of volume in nm^3: " + str(stats[0]))
+print("Standard deviation of radius in nm: " + str(stats[1]))
